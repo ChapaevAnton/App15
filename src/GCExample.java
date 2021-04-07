40d9a0abb2bd26ae825013cde24dfbf529eb8650
Chapaev Anton <virsus2009@gmail.com>
2021-04-07 19:38:03 +0700

    first commit

---
+++ src/GCExample.java
@@ -0,0 +1,25 @@
+public class GCExample {
+    static class Answer {
+        final int value = 42;
+    }
+
+    static class LifeUniverseEverything {
+        static final Answer ANSWER = new Answer();
+    }
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(
+                GCExample::findAnswerToLifeTheUniverseAndEverything, "thread1");
+        Thread thread2 = new Thread(() -> {
+            while (true) {
+            }
+        }, "thread2");
+        thread1.start();
+        thread2.start();
+    }
+
+    public static void findAnswerToLifeTheUniverseAndEverything() {
+        Answer answer = LifeUniverseEverything.ANSWER;
+        System.out.println(answer.value);
+    }
+}
---
+++ src/MyMemories.java
@@ -0,0 +1,14 @@
+public class MyMemories {
+
+    public static void main(String[] args) {
+
+        {
+            String str = "str";
+        }
+
+        String str;
+
+    }
+
+
+}
